765f98484847e9de0b171fe963457bd78921e8e8
Michael Fortuna <m4tuna@gmail.com>
2023-04-06 17:39:42 -0400

    add key listener for inputs and textareas on page

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,1 +1,10 @@
-console.log('collab?');
+console.log('collab broskizzzzz? 4');
+
+// Add a keyup event listener to the document to log the value of the currently focused input or textarea
+document.addEventListener("keyup", () => {
+    const activeElement = document.activeElement as HTMLInputElement | HTMLTextAreaElement;
+    // Check if the activeElement is an input or textarea element
+    if (activeElement.tagName === "INPUT" || activeElement.tagName === "TEXTAREA") {
+    console.log(activeElement.value);
+    }
+});
