2c7b4a63932e6fff259da2ab83a5c7a4553be089
Michael Fortuna <m4tuna@gmail.com>
2023-04-07 19:52:23 -0400

    updated form

--- src/options.ts
+++ src/options.ts
@@ -1,75 +1,15 @@
 import '../styles/options.scss';
 
-const textarea = document.querySelector('.custom-textarea') as HTMLTextAreaElement;
-const suggestionText = document.querySelector('.suggestion-text') as HTMLElement;
-const hiddenText = document.querySelector('.hidden-text') as HTMLElement;
-
-// Sample suggestions
-const suggestions: { [key: string]: string } = {
-    "hello": "hello world.",
-    "what": "what the actual fuck is going on in here?",
-};
-
-function getSuggestion(word: string): string {
-    return Object.prototype.hasOwnProperty.call(suggestions, word) ? suggestions[word] : '';
-}
-
-function escapeHTML(text: string): string {
-    const div = document.createElement('div');
-    div.textContent = text;
-    return div.innerHTML;
-}
-
-function getInputWidth(inputText: string): number {
-    hiddenText.textContent = inputText;
-    return hiddenText.getBoundingClientRect().width;
-}
-
-textarea.addEventListener('input', function () {
-    const words = textarea.value.split(' ');
-    const lastWord = words.slice(-1)[0] || '';
-    const suggestion = getSuggestion(lastWord);
-    
-    if (suggestion) {
-        const userText = escapeHTML(textarea.value);
-        const highlightedSuggestion = `<span style="color:#999">${escapeHTML(suggestion.substring(lastWord.length))}</span>`;
-        suggestionText.innerHTML = userText + highlightedSuggestion;
-        const inputWidth = getInputWidth(userText);
-        suggestionText.style.left = `${inputWidth}px`;
+window.addEventListener('load', () => {
+    const saveButton = document.getElementById('saveButton');
+    if (saveButton) {
+      saveButton.addEventListener('click', saveOptions);
     } else {
-        suggestionText.textContent = '';
+      console.error('Save button not found in the DOM');
     }
-});
+  });
 
-function getSuggestedValue(): string {
-    const words = textarea.value.split(' ');
-    const lastWord = words.slice(-1)[0] || '';
-    const suggestion = getSuggestion(lastWord);
-    return suggestion ? textarea.value.substring(0, textarea.selectionStart) + suggestion.substring(lastWord.length) : '';
-}
-
-let tabUsedForSuggestion = false;
-
-textarea.addEventListener('keydown', function (event) {
-    if (event.key === 'Tab') {
-        const suggestedValue = getSuggestedValue();
-        if (suggestedValue && !tabUsedForSuggestion) {
-            event.preventDefault(); // Prevent default behavior (moving focus to the next input element)
-            textarea.value = suggestedValue;
-            textarea.dispatchEvent(new Event('input')); // Trigger the input event to update the suggestion
-            tabUsedForSuggestion = true;
-        } else {
-            tabUsedForSuggestion = false;
-        }
-    } else {
-        tabUsedForSuggestion = false;
-    }
-});
-
-
-// form.ts
 function saveOptions(): void {
-    console.log("SAVE OPTIONS");
     const form = document.getElementById('optionsForm') as HTMLFormElement;
     const options = {
       model: (form.elements.namedItem('model') as HTMLSelectElement).value,
@@ -83,13 +23,8 @@ function saveOptions(): void {
     };
   
     localStorage.setItem('options', JSON.stringify(options));
-    alert('Options saved to localStorage.');
-  }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    const saveButton = document.getElementById('saveButton') as HTMLButtonElement;
-    saveButton.addEventListener('click', saveOptions);
-  });
-  
+    console.log('Options saved to localStorage.');
+}
+
   export { saveOptions };
   
