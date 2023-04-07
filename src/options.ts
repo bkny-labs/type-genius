76252fd3d03d818caddb905f3a48850ad0e2e475
Michael Fortuna <m4tuna@gmail.com>
2023-04-06 22:40:03 -0400

    add fake example of autocmplete style to options page for testing

--- src/options.ts
+++ src/options.ts
@@ -1,1 +1,42 @@
 import '../styles/options.scss';
+
+const textarea = document.querySelector('.custom-textarea') as HTMLTextAreaElement;
+const suggestionText = document.querySelector('.suggestion-text') as HTMLElement;
+const hiddenText = document.querySelector('.hidden-text') as HTMLElement;
+
+// Sample suggestions
+const suggestions: { [key: string]: string } = {
+    "hello": "hello world.",
+    "what": "what the actual fuck is going on in here?",
+};
+
+function getSuggestion(word: string): string {
+    return Object.prototype.hasOwnProperty.call(suggestions, word) ? suggestions[word] : '';
+}
+
+function escapeHTML(text: string): string {
+    const div = document.createElement('div');
+    div.textContent = text;
+    return div.innerHTML;
+}
+
+function getInputWidth(inputText: string): number {
+    hiddenText.textContent = inputText;
+    return hiddenText.getBoundingClientRect().width;
+}
+
+textarea.addEventListener('input', function () {
+    const words = textarea.value.split(' ');
+    const lastWord = words.slice(-1)[0] || '';
+    const suggestion = getSuggestion(lastWord);
+    
+    if (suggestion) {
+        const userText = escapeHTML(textarea.value);
+        const highlightedSuggestion = `<span style="color:#999">${escapeHTML(suggestion.substring(lastWord.length))}</span>`;
+        suggestionText.innerHTML = userText + highlightedSuggestion;
+        const inputWidth = getInputWidth(userText);
+        suggestionText.style.left = `${inputWidth}px`;
+    } else {
+        suggestionText.textContent = '';
+    }
+});
