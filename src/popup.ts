ba1854ee5e5188b0a5d27c33ae0ec1a9c7d9bafa
Michael Fortuna <m4tuna@gmail.com>
2023-04-11 10:15:23 -0400

    fixed logic

--- src/popup.ts
+++ src/popup.ts
@@ -10,10 +10,16 @@ const enableCheckbox = document.getElementById('toggle') as HTMLInputElement;
 let keyCheck = false;
 
 enableCheckbox.addEventListener('change', () => {
+  document.getElementById('alertMessage').style.display = 'none';
+  document.getElementById('connectedMessage').style.display = 'none';
   if (keyCheck === true) {
     setStorageItem('typeGeniusEnabled', enableCheckbox.checked);
     refreshData();
-    document.getElementById('alertMessage').style.display = undefined;
+    if (enableCheckbox.checked) {
+      document.getElementById('connectedMessage').style.display = 'block';
+    } else {
+      document.getElementById('connectedMessage').style.display = 'none';
+    }
   } else {
     enableCheckbox.checked = false;
     document.getElementById('alertMessage').style.display = 'block';
